378e5d376106a272bb4db9e43966231d9db99bf7
Frank <frankcamilogordillopea@gmail.com>
2024-09-03 21:48:04 -0500

    Class CuentaBancaria + test CuentaBancaria
    Se añadio la clase CuentaBancaria, constructor y metodos requeridos. Y se implementaron dos pruebas unitarias por cada metodo de la misma clase.

--- src/cuenta_bancaria.py
+++ src/cuenta_bancaria.py
@@ -1,2 +1,41 @@
 class CuentaBancaria:
-    # TODO: Aquí va el código de la clase CuentaBancaria
+    def __init__(self, titular, numeroCuenta, saldo):
+        self.titular = titular
+        self.numeroCuenta = numeroCuenta
+        self.saldo = saldo
+        self.tipoInteres = 1.5
+
+    def getTitular(self):
+        return self.titular
+
+    def setTitular(self, titular):
+        self.titular = titular
+    
+    def getNumeroCuenta(self):
+        return self.numeroCuenta
+
+    def getSaldo(self):
+        return self.saldo
+    
+    def ingresar(self, cantidad):
+        if(cantidad <= 0):
+            print("El valor para ingresar es invalido.")
+        else:
+            self.saldo += cantidad
+
+    def retirar(self, cantidad):
+        if(cantidad <= 0 or cantidad > self.getSaldo()):
+            print("El saldo es menor a la cantidad solicitada, o el valor a retirar es invalido")
+        else:
+            self.saldo -= cantidad
+    
+    def calcularInteres(self):
+        return self.saldo + (self.saldo*self.tipoInteres/100)
+
+    def setTipoInteres(self, tipoInteres):
+        if(tipoInteres < 0 or tipoInteres > 10):
+            print("Cantidad de interes ingresada es invalida")
+        else:
+            self.tipoInteres = tipoInteres
+
+
--- tests/test_cuenta_bancaria.py
+++ tests/test_cuenta_bancaria.py
@@ -5,11 +5,64 @@ import unittest
 # Add the 'src' directory to the Python path so 'calculadora' can be imported
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
-
 class TestCalcular(unittest.TestCase):
-    # TODO Adiciona tus pruebas unitarias aquí.
-    # Ejemplo:   
-    # def test_suma(self):
-    #     valor_esperado = 3
-    #     valor_actual = calcular(1, 2, '+')
-    #     self.assertEqual(valor_esperado, valor_actual)
+    
+    def setUp(self):
+        self.cuenta1 = CuentaBancaria("Jhon Doe", "505051", 0)
+        self.cuenta2 = CuentaBancaria("Mark Doe", "500001",50000)
+    
+    def test_getTitular1(self):
+        self.assertEqual(self.cuenta1.getTitular(), "Jhon Doe")
+
+    def test_getTitular2(self):
+        self.assertEqual(self.cuenta2.getTitular(), "Mark Doe")
+
+    def test_setTitular1(self):
+        self.cuenta1.setTitular("N N")
+        self.assertEqual(self.cuenta1.getTitular(), "N N")
+
+    def test_setTitular2(self):
+        self.cuenta2.setTitular("")
+        self.assertEqual(self.cuenta2.getTitular(), "")
+
+    def test_getNumeroCuenta1(self):
+        self.assertEqual(self.cuenta1.getNumeroCuenta(), "505051")
+
+    def test_getNumeroCuenta2(self):
+        self.assertEqual(self.cuenta2.getNumeroCuenta(), "500001")
+
+    def test_getSaldoCuenta1(self):
+        self.assertEqual(self.cuenta1.getSaldo(), 0)
+
+    def test_getSaldoCuenta2(self):
+        self.assertEqual(self.cuenta2.getSaldo(), 50000)
+
+    def test_getingresarCuenta1(self):
+        self.cuenta1.ingresar(-1)
+        self.assertEqual(self.cuenta1.getSaldo(), 0)
+
+    def test_getingresarCuenta2(self):
+        self.cuenta2.ingresar(10000)
+        self.assertEqual(self.cuenta2.getSaldo(), 60000)
+
+    def test_getretirarCuenta1(self):
+        self.cuenta1.retirar(1)
+        self.assertEqual(self.cuenta1.getSaldo(), 0)
+
+    def test_getretirarCuenta2(self):
+        self.cuenta2.ingresar(10000)
+        self.assertEqual(self.cuenta2.getSaldo(), 40000)
+
+    def test_getCalcularInteresCuenta1(self):
+        self.assertEqual(self.cuenta1.calcularInteres(), 0)
+
+    def test_getCalcularInteresCuenta2(self):
+        self.assertEqual(self.cuenta2.calcularInteres(), 50000+(50000*1.5/100))
+
+    def test_getSetTipoInteresCuenta1(self):
+        self.cuenta1.setTipoInteres(11)
+        self.assertEqual(self.cuenta1.calcularInteres(), 0)
+
+    def test_getSetTipoInteresCuenta2(self):
+        self.cuenta2.ingresar(10)
+        self.assertEqual(self.cuenta2.calcularInteres, 50000+(50000*10/100))
